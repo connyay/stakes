34913a5cab741d6c4b0d767dfbedef787dbbf279
Connor Hindley <conn.hindley@gmail.com>
2014-07-06 18:47:59 -0400

    add some sample charts

--- client/app/components/Dashboard/controllers.js
+++ client/app/components/Dashboard/controllers.js
@@ -1,7 +1,23 @@
 (function() {
     'use strict';
 
-    angular.module('stakes.dashboard.controllers', [])
-        .controller('DashboardCtrl', function($scope) {});
+    angular.module('stakes.dashboard.controllers', ['angles'])
+        .controller('DashboardCtrl', function($scope) {
+            $scope.chart = {
+                labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                datasets: [{
+                    fillColor: 'rgba(151,187,205,0)',
+                    strokeColor: '#92cf5c',
+                    pointColor: 'rgba(151,187,205,0)',
+                    pointStrokeColor: '#92cf5c',
+                    data: [3, 6, 2, 4, 12, 20, 15]
+                }],
+            };
+            $scope.options = {
+                segmentShowStroke: false,
+                responsive: true,
+                tooltipTemplate: '<%= value %> new accounts',
+            };
+        });
 
 })();
